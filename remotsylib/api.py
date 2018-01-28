85048e417ac8b286eaf998e67cfb2108c0ef1dc9
jorgecis <jorgecis@gmail.com>
2018-01-28 16:32:16 -0600

    Pylint score 10

--- remotsylib/api.py
+++ remotsylib/api.py
@@ -1,60 +1,64 @@
+#!/bin/env python2
+""" Remotsy library for python
+    Copyright 2018 Jorge Cisneros jorge@remotsy.com
+"""
 from json import dumps, loads
-from sys import exit
 from urllib2 import Request, urlopen, HTTPError, URLError
+import sys
 
 
-class API():
-    
-    def __init__(self, apiurl = "https://remotsy.com/rest/"):
+class API(object):
+    """ API class for the remotsy lib core """
+    def __init__(self, apiurl="https://remotsy.com/rest/"):
         self.apiurl = apiurl
         self.auth_key = None
 
     def post(self, url, data):
+        """ Generic function to do a http post """
         req = Request(self.apiurl + url)
         req.add_header('Content-Type', 'application/json')
         if self.auth_key is not None:
-           data["auth_key"] = self.auth_key
+            data["auth_key"] = self.auth_key
         try:
             resp = urlopen(req, dumps(data))
-        except HTTPError as e:
-            print e
-            exit(-1)
-        except URLError as e:
-            print e
-            exit(-2)
+        except HTTPError as errobj:
+            print errobj
+            sys.exit(-1)
+        except URLError as errobj:
+            print errobj
+            sys.exit(-2)
         else:
             body = loads(resp.read())
             return  body
 
     def login(self, username, password):
+        """ Function to do the login and return a auth token """
         data = {"username": username,
                 "password": password}
 
         self.auth_key = None
-        ret = self.post("session/login",  data)
+        ret = self.post("session/login", data)
         if ret["status"] == "success":
             self.auth_key = ret["data"]["auth_key"]
         return self.auth_key
 
     def list_controls(self):
-        ret = self.post("controls/list",{})
+        """ Function to get the list of the remotsy controls """
+        ret = self.post("controls/list", {})
         if ret["status"] == "success":
             return ret["data"]["controls"]
         else:
             return None
 
     def list_buttons(self, idctl):
-        ret = self.post("controls/get_buttons_control",{"id_control": idctl})
+        """ Function to get the list of the button of a control """
+        ret = self.post("controls/get_buttons_control", {"id_control": idctl})
         if ret["status"] == "success":
             return ret["data"]["buttons"]
         else:
             return None
 
-    def blast(self, iddev, idbto, ntime = 1):
-        ret = self.post("codes/blast",{"id_dev": iddev, "code": idbto, "ntime": ntime})
-        if ret["status"] == "success":
-            return True
-        else:
-            return False
-
-
+    def blast(self, iddev, idbto, ntime=1):
+        """ Function to blast infrared code via remotsy """
+        ret = self.post("codes/blast", {"id_dev": iddev, "code": idbto, "ntime": ntime})
+        return ret["status"] == "success"
