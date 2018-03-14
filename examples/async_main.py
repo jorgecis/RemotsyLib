e20e60de75e08646ab99970276ed61e4fd745a6e
Jasmit Tarang <jaytarang92@gmail.com>
2018-03-14 02:54:02 -0500

    more pylint changes

--- examples/async_main.py
+++ examples/async_main.py
@@ -1,50 +1,57 @@
 # /usr/bin/python3.6
+""" Remotsy async Python API """
 from asyncio import get_event_loop
 from aiohttp.web import (Application, run_app, View, json_response)
 from remotsylib.api_async import API
 
-remotsy = API()
-
 
 class Login(View):
+    """ Our login function"""
+
     async def post(self):
+        """ POST """
         args = await self.request.json()
         username_arg = args.get('username', None)
         password_arg = args.get('password', None)
-        self.request.app['remotsy_auth'] = await remotsy.login(auth=dict(
+        self.request.app['remotsy_auth'] = await API().login(auth=dict(
             username=username_arg, password=password_arg))
         return json_response(self.request.app['remotsy_auth'], status=200)
 
 
 class ListControls(View):
+    """ Lists all the Controls """
+
     async def post(self):
-        data = dict(auth_key=self.request.app['remotsy_auth'])
-        controls = await remotsy.list_controls()
+        """ POST """
+        #data = dict(auth_key=self.request.app['remotsy_auth'])
+        controls = await API().list_controls()
         return json_response(controls, status=200)
 
 
 def bind_routes_to_server(app):
+    """ Binds all the routes to the server """
     app.router.add_post('/login', Login)
-    app.router.add_post('/list_controls', List_Controls)
+    app.router.add_post('/list_controls', ListControls)
 
 
 def make_server(close=False):
+    """ Creates the server """
+
     app = Application()
     app['remotsy_auth'] = None
     bind_routes_to_server(app)
     loop = get_event_loop()
-    loop.close() if close else None
+    _ = loop.close() if close else None
     loop.create_server(run_app(app, port=8080))
     loop.run_forever()
 
 
-def main(): 
+def main():
+    """ runs the server """
     try:
         make_server()
+
     except KeyboardInterrupt:
-        make_server(close=True)
-    except Exception as e:
-        print('Application Error: {}'.format(e))
         make_server(close=True)
 
 if __name__ == "__main__":
--- remotsylib/api_async.py
+++ remotsylib/api_async.py
@@ -1,18 +1,21 @@
 # /usr/bin/python3.6
-from aiohttp import (ClientSession, TCPConnector, BasicAuth)
-from asyncio import get_event_loop
+""" Python3+ async Remotsy API """
+from aiohttp import ClientSession
 from yarl import urljoin
 
 
 class Requests():
+    """ Generic Get/Post request client """
 
     async def do_get(self, url, data=None):
+        """ Get request """
         async with ClientSession() as session:
             async with session.get(url, json=data) as response:
                 j_resp = await response.json()
                 return j_resp
 
     async def do_post(self, url, data=None):
+        """ Post request """
         async with ClientSession() as session:
             async with session.post(url, json=data) as response:
                 j_resp = await response.json()
@@ -20,6 +23,7 @@ class Requests():
 
 
 class API():
+    """ This is the main API  """
 
     def __init__(self, api_url='https://remotsy.com/rest/'):
         self.api_url = api_url
@@ -27,6 +31,7 @@ class API():
         self.auth_key = None
 
     async def post(self, partial_url=None, data=None):
+        """ Carries out the request to Remotsy's server """
         try:
             if partial_url is not None:
                 if self.auth_key is not None:
@@ -34,11 +39,12 @@ class API():
                 endpoint = urljoin(self.api_url, partial_url)
                 response = await self.requests.do_post(url=endpoint, data=data)
                 return response
-        except Exception as e:
-            return dict(error='API Error', message=e, status=409)
+        except Exception as error:
+            return dict(error='API Error', message=error, status=409)
 
-    async def login(self, auth=dict(username=None, password=None)):
-        if auth['username'] is not None and auth['password'] is not None:
+    async def login(self, auth=None):
+        """ Login and recieve a api key """
+        if auth is not None:
             login_status = await self.post(
                 partial_url='session/login', data=auth)
             print(login_status)
@@ -48,6 +54,7 @@ class API():
         return dict(error='Login', message='No credentials!', status=401)
 
     async def list_controls(self):
+        """ List all the controls available """
         controls_list = await self.post('controls/list', data={})
         if controls_list['status'] == 'success':
             return controls_list['data']['controls']
@@ -55,6 +62,7 @@ class API():
             error='List Controls', message='No controls found', status=409)
 
     async def list_buttons(self, controller_id):
+        """  List all buttons for a specific controller """
         buttons = await self.post(
             'controls/get_buttons_control', dict(id_control=controller_id))
         if buttons['status'] == 'success':
@@ -63,6 +71,7 @@ class API():
             error='List Buttons', message='No Controller ID', status=409)
 
     async def blast(self, device_id, button_id, ntime=1):
+        """ Blast a button to a device nTimes """
         blast_resp = self.post(
             'codes/blast', dict(id_dev=device_id, code=button_id, ntime=ntime))
         if blast_resp['status'] == 'success':
@@ -70,24 +79,28 @@ class API():
         return dict(error='IR Blast', message=False, status=409)
 
     async def list_routines(self):
+        """ List all the routines available """
         routines = self.post('routines/list', data={})
         if routines['status'] == 'success':
             return routines
         return dict(error='List Routines', message=False, status=409)
 
     async def play_routine(self, routine_id):
+        """ Play a specific routine """
         played = self.post('routines/play_routine', dict(idroutine=routine_id))
         if played['status'] == 'success':
             return played
         return dict(error='Play Routine', message=False, status=409)
 
     async def blink_led(self, device_id):
+        """ blink Remotsy's led """
         blink = self.post('devices/blink', dict(id_dev=device_id))
         if blink['status'] == 'success':
             return blink
         return dict(error='Blink LED', message=False, status=409)
 
     async def update_firmware(self, device_id):
+        """ Update Remotsy's firmware """
         fw_update = self.post('devices/updatefirmware', dict(id_dev=device_id))
         if fw_update['status'] == 'success':
             return fw_update
