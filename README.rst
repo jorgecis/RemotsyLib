3cf10c5f79e4965339b7b9657f4c3853f2a130c1
Ubuntu <ubuntu@ip-172-26-13-215.ec2.internal>
2019-02-21 03:31:36 +0000

    Get better readme descriptiony

--- README.rst
+++ README.rst
@@ -1,2 +1,36 @@
-full readme
+Remotsy python library |Build Status| |Codacy Badge|
+=========================================
+
+`Remotsy <https://www.remotsy.com>`_ is an infrared blaster device, is cloud controlled,
+this a Python library to control the Remotsy device via Rest API.
+
+Installation
+============
+
+  $ pip install remotsylib
+
+Examples
+========
+
+See the directory `examples  <https://github.com/jorgecis/RemotsyLib/tree/master/examples>`_.
+
+Authentication
+==============
+
+ You can use your remotsy username and password, but for security is recomended to generate
+a application password, logon in https://home.remotsy.com and use the option App Passwords.
+
+
+Documentation API
+-----------------
+
+The API documentation and links to additional resources are available at
+https://www.remotsy.com/help
+
+
+.. |Build Status| image:: https://travis-ci.org/jorgecis/RemotsyLib.svg?branch=master
+   :target: https://travis-ci.org/jorgecis/RemotsyLib
+.. |Codacy Badge| image:: https://api.codacy.com/project/badge/Grade/79fb3255b464442983bb5b6b6fdecd98
+   :target: https://app.codacy.com/app/jorgecis/RemotsyLib?utm_source=github.com&utm_medium=referral&utm_content=jorgecis/RemotsyLib&utm_campaign=Badge_Grade_Settings
+
 
--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from os import path
 from codecs import open as openf
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with openf(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='remotsylib',
     version='0.0.1',
-    description='A Remotsy lib for use the Restfull API',
+    description='A Remotsy python lib for use the Restfull API',
     long_description=long_description,
     url='https://github.com/jorgecis/remotsylib',
     author='Jorge Cisneros',
@@ -25,13 +22,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords='Remotsy infrared remote control smarthome',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    #install_requires=['peppercorn'],  # Optional
-    #extras_require={  # Optional
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
 )
